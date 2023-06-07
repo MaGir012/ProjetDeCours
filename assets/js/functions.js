dba6c6b9ce1cbfbde2a792b72452f626315b2582
Maxime Girard <maximegirard.012@gmail.com>
2023-06-07 12:57:05 -0400

    Implémentation de la fonction smoothScroll

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -10,6 +10,23 @@ function nav(){
 	
 }
 
+function smoothScroll(target) {
+	const element = document.querySelector(target);
+	window.scrollTo({
+	  behavior: 'smooth',
+	  top: element.offsetTop
+	});
+  }
+
 nav();
+const links = document.querySelectorAll('.menu-link');
+
+links.forEach(link => {
+  link.addEventListener('click', (event) => {
+    event.preventDefault();
+    const target = event.target.getAttribute('href');
+    smoothScroll(target);
+  });
+});
 
 });
